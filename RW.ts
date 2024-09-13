b2f42f10e338fffae4977ea628c3a98a663e211d
Michael James <micrjames921@gmail.com>
2024-09-13 14:42:44 -0700

    blocked out class and setup initial tests

--- RW.ts
+++ RW.ts
@@ -1,4 +1,32 @@
+import fs from "fs";
+
 export class RW {
-   constructor() {
+   private _fd: number;
+   private flag: string;
+   private fileName: string;
+   private _data: string;
+   private _stats: fs.Stats;
+   constructor(fileName: (string | null) = null, append: boolean = false) {
+	  this.flag = append ? 'a+' : 'r+'; 
+	  // r+ : reading & writing; exception if file does not exist
+	  // a+ : reading & appending; file created if not found
+	  this.fileName = fileName;
+	  this._data = null;
+	  this._fd = -1;
+	  this._stats = null;
+   }
+   readStats(): fs.Stats {
+	  if(this.fileName)
+		 this._stats = fs.statSync(this.fileName);
+	  return this._stats;
+   }
+   get fd(): number {
+	  return this._fd;
+   }
+   get data(): string {
+	  return this._data;
+   }
+   get stats(): fs.Stats {
+	  return this._stats;
    }
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,12 +1,55 @@
 import { RW } from "../RW"
 
-describe("Reading and Writing files", () => {
-   beforeAll(() => {
+describe("Reading Files", () => {
+   let rw: RW;
+   let filename: string;
+   describe("Without Reading", () => {
+	  beforeAll(() => {
+		 rw = new RW();
+	  });
+	  test("Should exist.", () => {
+		 expect(rw).not.toBeNull();
+	  });
+	  test("Should not have a 'fd'", () => {
+		 expect(rw.fd).toBe(-1);
+	  });
+	  test("Should not have any 'stats'", () => {
+		 expect(rw.stats).toBeNull();
+	  });
+	  test("Should not have any 'data'", () => {
+		 expect(rw.data).toBeNull();
+	  });
    });
-   describe("Reading files", () => {
-	  test.todo("Should ...");
-	  test.todo("Should ...");
-	  test.todo("Should ...");
-	  test.todo("Should ...");
+   describe("With Reading", () => {
+	  beforeAll(() => {
+		 filename = __dirname + "/file.txt";
+		 rw = new RW(filename);
+	  });
+	  test("Should exist.", () => {
+		 expect(rw).not.toBeNull();
+	  });
+	  test.todo("Should not have a 'fd'");
+	  test.todo("Should not have any 'stats'");
+	  test.todo("Should not have any 'data'");
    });
 });
+	  /*
+		  if(err === null) {
+			 this._stats = stats;
+			 fs.open(this.fileName, this.flag, (err, fd) => {
+				if(err === null) {
+				   this._fd = fd;
+				} else if(err.code === 'ENONT') {
+				   console.log("file does not exist");
+				} else {
+				   console.log("some other error: err.code");
+				}
+			 });
+		  } else if(err.code === 'ENONT') {
+			  // file does not exist
+			  console.log("file does not exist");
+		  } else {
+			  // some other error: err.code
+			  console.log("some other error: err.code");
+		  }
+	  */
